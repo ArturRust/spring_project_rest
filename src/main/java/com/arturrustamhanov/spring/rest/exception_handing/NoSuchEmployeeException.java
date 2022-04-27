e927d659bc85e966e8d876b1697b037f78e91db8
Artur Rustamhanov <dagsardar01@gmail.com>
2022-04-27 11:58:26 +0300

    Initial commit

---
+++ src/main/java/com/arturrustamhanov/spring/rest/exception_handing/NoSuchEmployeeException.java
@@ -0,0 +1,8 @@
+package com.arturrustamhanov.spring.rest.exception_handing;
+
+public class NoSuchEmployeeException extends RuntimeException{
+
+    public NoSuchEmployeeException(String message) {
+        super(message);
+    }
+}
